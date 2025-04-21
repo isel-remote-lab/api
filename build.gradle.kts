aab25cb428c0ed02b4018164acaf84caa791a873
Antonio Alves <a50539@alunos.isel.pt>
2025-04-21 17:59:25 +0100

    Add Docker-related tasks and configurations for building images and managing containers

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,3 +91,222 @@ project(":host") {
         enabled = true
     }
 }
+
+/**
+ * Docker related tasks
+ */
+task<Copy>("extractUberJar") {
+    dependsOn("assemble")
+    // opens the JAR containing everything...
+    from(zipTree(layout.buildDirectory.file("libs/host-$version.jar").get().toString()))
+    // ... into the 'build/dependency' folder
+    into("build/dependency")
+}
+
+/**
+ * Docker images names
+ */
+data class DockerImages(
+    val dbTests: String,
+    val postgresDev: String,
+    val postgresProd: String,
+    val jvm: String,
+    val nodeDev: String,
+    val nodeProd: String,
+    //val nginx: String,
+)
+
+val dockerImages =
+    DockerImages(
+        dbTests = "db-tests",
+        postgresDev = "rl-postgres-dev",
+        postgresProd = "rl-postgres-prod",
+        jvm = "rl-jvm",
+        nodeDev = "rl-node-dev",
+        nodeProd = "rl-node-prod",
+        //nginx = "rl-nginx",
+    )
+
+object DockerFiles {
+    private const val DOCKER_FOLDER = "docker"
+    private const val DOCKER_DB_FOLDER = "../repository-jdbi/docker"
+    const val DOCKER_COMPOSE = "docker-compose.yml"
+
+    const val JVM = "$DOCKER_FOLDER/Dockerfile-jvm"
+    const val DB_TESTS = "$DOCKER_DB_FOLDER/Dockerfile-db-tests"
+    const val POSTGRES_DEV = "$DOCKER_FOLDER/Dockerfile-postgres"
+    const val POSTGRES_PROD = "$DOCKER_FOLDER/Dockerfile-postgres"
+    const val NODE_DEV = "$DOCKER_FOLDER/Dockerfile-node-dev"
+    const val NODE_PROD = "$DOCKER_FOLDER/Dockerfile-node-prod"
+}
+
+
+/**
+ * Build tasks
+ */
+task<Exec>("buildImageJvm") {
+    dependsOn("extractUberJar")
+    commandLine("docker", "build", "-t", dockerImages.jvm, "-f", DockerFiles.JVM, ".")
+}
+
+task<Exec>("buildImageDbTests") {
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        dockerImages.dbTests,
+        "-f",
+        DockerFiles.DB_TESTS,
+        "../repository-jdbi",
+    )
+}
+
+task<Exec>("buildImagePostgresDev") {
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        dockerImages.postgresDev,
+        "-f",
+        DockerFiles.POSTGRES_DEV,
+        "../repository-jdbi",
+    )
+}
+
+task<Exec>("buildImagePostgresProd") {
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        dockerImages.postgresProd,
+        "-f",
+        DockerFiles.POSTGRES_PROD,
+        "../repository-jdbi",
+    )
+}
+
+task<Exec>("buildImageNodeDev") {
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        dockerImages.nodeDev,
+        "-f",
+        DockerFiles.NODE_DEV,
+        "../../../js",
+    )
+}
+
+task<Exec>("buildImageNodeProd") {
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        dockerImages.nodeProd,
+        "-f",
+        DockerFiles.NODE_PROD,
+        "../../../js",
+    )
+}
+
+/*
+task<Exec>("buildImageNginx") {
+    commandLine(
+        "docker",
+        "build",
+        "-t",
+        dockerImages.nginx,
+        "-f",
+        "docker/Dockerfile-nginx",
+        "../../../",
+    )
+    dependsOn("webpackProd")
+}
+
+ */
+
+task("buildImageAllDev") {
+    dependsOn("buildImageJvm")
+    dependsOn("buildImagePostgresDev")
+    dependsOn("buildImageNodeDev")
+    //dependsOn("buildImageNginx")
+}
+
+task("buildBackendImageAll") {
+    dependsOn("buildImageJvm")
+    dependsOn("buildImagePostgresDev")
+}
+
+/**
+ * Webpack tasks
+ */
+task<Exec>("webpackProd") {
+    workingDir = file("../../../js")
+    commandLine("npm", "run", "build")
+}
+
+/**
+ * Docker compose tasks
+ */
+task<Exec>("allUp") {
+    dependsOn("buildImageAll")
+    commandLine("docker", "compose", "up", "-d")
+    finalizedBy("ngrokUp")
+}
+
+task<Exec>("allDown") {
+    commandLine("docker", "compose", "down")
+    finalizedBy("ngrokDown")
+}
+
+task<Exec>("backendAllUp") {
+    dependsOn("buildBackendImageAll")
+    commandLine("docker", "compose", "up", "-d")
+    finalizedBy("ngrokUp")
+}
+
+task<Exec>("backendAllDown") {
+    commandLine("docker", "compose", "down")
+    finalizedBy("ngrokDown")
+}
+
+task<Exec>("dbTestsUp") {
+    dependsOn("buildImageDbTests")
+    workingDir = file("../")
+    commandLine("docker", "compose", "up", "-d")
+}
+
+task<Exec>("dbTestsDown") {
+    commandLine("docker", "compose", "down", dockerImages.dbTests)
+}
+
+/**
+ * Ngrok tasks
+ */
+task<Exec>("ngrokUp") {
+    commandLine("docker", "run", "-d", "--net=host", "-e",
+        "NGROK_AUTHTOKEN=2qV7kqaFqYEKQCDmV939PgrdsT3_2Mq2EbxAvtRHc8L28DM9w", "ngrok/ngrok:latest", "http",
+        "--url=awaited-louse-elegant.ngrok-free.app", "8080")
+}
+
+task<Exec>("ngrokDown") {
+    commandLine("ngrok", "kill")
+}
+
+/**
+ * Test tasks
+ */
+task<Exec>("testBackend") {
+    dependsOn("dbTestsUp")
+    dependsOn("buildImageJvm")
+    commandLine("docker", "compose", "exec", dockerImages.jvm, "test")
+    finalizedBy("dbTestsDown")
+}
+
+/**
+ * Logs tasks
+ */
+task<Exec>("showLogs") {
+    commandLine("docker", "compose", "logs", "-f")
+}
+
